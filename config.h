46f8468b84aa0af142084039b7cb19c3df9060d8
Franc[e]sco <lolisamurai@tfwno.gf>
2020-10-20 22:10:01 +0200

    enable emojis

--- config.h
+++ config.h
@@ -5,7 +5,11 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "scientifica:pixelsize=11" };
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const char *fonts[]          = {
+	"scientifica:pixelsize=11",
+	"emoji:pixelsize=11:antialias=true:autohint=true",
+};
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { "#bbbbbb", "#222222", "#444444" },
